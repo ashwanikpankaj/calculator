63a9dec7d1e80eb82c9153e3be942f5fe7dd1834
ashwanikpankaj <ashnit8294@gmail.com>
2022-01-16 18:48:30 +0530

    my calci

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -65,7 +65,7 @@ export const Calculator = () => {
                 <button className="btn btn-danger" onClick={()=>dispatch(loadButton(2))}>2</button>
               </div>
               <div className="col-4">
-                <button className="btn btn-danger" onClick={()=>dispatch(loadButton(1))}>1</button>
+                <button className="btn btn-danger" onClick={()=>dispatch(loadButton(1))}>0</button>
               </div>
             </div>
           </div>
